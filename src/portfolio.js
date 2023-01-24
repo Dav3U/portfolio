e9fe8b28974f8ba73c919b4d789dc885c9bb2f9c
Dave3U <davidujile@icloud.com>
2023-01-24 07:22:18 +0100

    added listener function to navbar at 720px screensize

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,11 +3,62 @@ maindiv= document.getElementById('main')
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('collapsedNavbar')
 const bsCollapse = new bootstrap.Collapse(menuToggle,{toggle:false})
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
 
-// Navbar autoclose on main poage click
-// maindiv.addEventListener('click', () => { bsCollapse.toggle() })
 
+  var x = window.matchMedia("(max-width: 720px)")
+
+
+  function myFunction(x) {
+    if (x.matches) { // If media query matches
+
+// Navbar autoclose on main page click
+
+        window.addEventListener('click', function(e){   
+            if (document.getElementById('nav').contains(e.target)){
+              // Clicked in box
+            //   window.alert("click inside")
+        
+              navLinks.forEach((l) => {
+                l.addEventListener('click', 
+                () => { bsCollapse.toggle() 
+                } )
+        
+            console.log(bsCollapse);
+            bsCollapse._config.toggle = true;
+            // menuToggle.classList.add('down')
+             
+            }
+            )
+        2
+        
+            } 
+            else{
+              // Clicked outside the box
+            //   bsCollapse.toggle()
+            if(bsCollapse._config.toggle==true)
+            {
+        
+            bsCollapse.toggle();
+            bsCollapse._config.toggle = false;
+        
+            }
+
+        
+        
+            //   window.alert("click outside")
+        
+            }
+          });
+        
+
+
+
+
+    } else {
+    //   document.body.style.backgroundColor = "pink";
+    }
+  }
+  
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
 
