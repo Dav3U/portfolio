955784b5ba61078eaf02d1419682ff19c47e1596
Dave3U <davidujile@icloud.com>
2023-02-05 21:10:00 +0100

    added row borders, restructured website directory

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # portfolio
 My portfolio page
+Built using the bootstrap framework, this portolio webesite depicts the flavours of simplicity and responsiveness which are important features of my websites.
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,6 @@
 // Navbar autoclose on navlink click
-maindiv= document.getElementById('main')
+const maindiv= document.getElementById('main')
+const nav= document.getElementById('nav')
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('collapsedNavbar')
 const skillsWrapper = document.getElementById('skills-wrapper')
@@ -14,55 +15,59 @@ const bsCollapse = new bootstrap.Collapse(menuToggle,{toggle:false})
 
   function mediaQueryToggle(x) {
     if (x.matches) { // If media query matches
-        navBarResponse();
-       
+        navBarResponse();       
+        console.log(window.innerWidth)
   }
   }
+
+  function setSticky(){
+    if (window.innerWidth <= 991){
+      nav.classList.add('sticky-top');
+    } 
+    else if(window.innerWidth > 991)
+    {
+      nav.classList.remove('sticky-top');
+    } 
+    window.onresize = function() {
+      if (window.innerWidth <= 991){
+        nav.classList.add('sticky-top');
+      } 
+      else if(window.innerWidth > 991)
+      {
+        nav.classList.remove('sticky-top');
+      } 
+    };
+  }
+
   function navBarResponse(){
     
     // Navbar autoclose on main page click
     
             window.addEventListener('click', function(e){   
                 if (document.getElementById('nav').contains(e.target)){
+
                   // Clicked in box
                 //   window.alert("click inside")
             
                   navLinks.forEach((l) => {
                     l.addEventListener('click', 
                     () => { bsCollapse.toggle() 
+                    bsCollapse._config.toggle = false;
                     } )
-            
-    
-                bsCollapse._config.toggle = true;
-    
-                 
                 }
                 )
-            
-            
                 } 
                 else{
-                  // Clicked outside the box
+                  console.log("Clicked outside the box");
                 //   bsCollapse.toggle()
                 if(bsCollapse._config.toggle==true)
                 {
-            
                 bsCollapse.toggle();
                 bsCollapse._config.toggle = false;
-            
                 }
-    
-            
-            
-                //   window.alert("click outside")
-            
                 }
               });
         } 
-
-  mediaQueryToggle(x) // Call listener function at run time
-  x.addListener(mediaQueryToggle) // Attach listener function on state changes
-
 
   function hideSkillIcons(y) {
     
@@ -75,5 +80,10 @@ const bsCollapse = new bootstrap.Collapse(menuToggle,{toggle:false})
     }
 }
 
+mediaQueryToggle(x) // Call listener function at run time
+x.addListener(mediaQueryToggle) // Attach listener function on state changes
+
 hideSkillIcons(y);
 y.addListener(hideSkillIcons);
+
+setSticky();
